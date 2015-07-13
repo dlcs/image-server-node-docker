ec81c07b8b0296209d94bf21d89aa107d196a0b8
Adam Christie <fractos@gmail.com>
2015-07-13 11:23:32 +0000

    tweak to operations.sh

--- operations.sh
+++ operations.sh
@@ -4,6 +4,8 @@ aws s3 cp $KAKADULOCATION /opt/kakadu.tar.gz
 
 cd /opt && tar -xzvf kakadu.tar.gz
 
-lighttpd -D -f /etc/lighttpd/lighttpd-1.conf
+lighttpd -f /etc/lighttpd/lighttpd-1.conf
 
-while true; sleep 1; done
+while true; do sleep 10; done
+
+
