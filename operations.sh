4b515ffc50026f2e3fdc71469c20383327e37101
Adam Christie <fractos@gmail.com>
2016-10-05 13:16:19 +0000

    build against kakadu-7.8. explictly adding mod_fastcgi to lighttpd configuration. explicitly setting LD_LIBRARY_PATH for Kakadu.

--- operations.sh
+++ operations.sh
@@ -4,6 +4,8 @@ aws s3 cp $KAKADULOCATION /opt/kakadu.tar.gz
 
 cd /opt && tar -xzvf kakadu.tar.gz
 
+export LD_LIBRARY_PATH=/opt/kakadu/kakadu-7.8/lib/Linux-x86-64-gcc
+
 lighttpd -f /etc/lighttpd/lighttpd-1.conf
 
 while true; do sleep 10; done
