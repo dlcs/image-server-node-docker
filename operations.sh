60ea83e2148ed286fb57de4f6e10e629f8f764bd
Adam Christie <fractos@gmail.com>
2017-03-30 15:47:31 +0000

    kakadu 7.9 support and automatically tail iipsrv logs.

--- operations.sh
+++ operations.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
+echo copying kakadu from s3...
 aws s3 cp $KAKADULOCATION /opt/kakadu.tar.gz
 
+echo extracting kakadu...
 cd /opt && tar -xzvf kakadu.tar.gz
 
-export LD_LIBRARY_PATH=/opt/kakadu/kakadu-7.8/lib/Linux-x86-64-gcc
+export LD_LIBRARY_PATH=/opt/kakadu/kakadu-7.9/lib/Linux-x86-64-gcc
 
+echo touching initial iipsrv log file
+touch /tmp/iipsrv-1.log
+chown www-data:www-data /tmp/iipsrv-1.log
+
+echo starting lighttpd
 lighttpd -f /etc/lighttpd/lighttpd-1.conf
 
-while true; do sleep 10; done
-
-
+echo tailing log...
+tail -f /tmp/iipsrv-1.log
