3dfeb68494fdda147bff1429bd82beee912f4bb3
Adam Christie <fractos@gmail.com>
2019-05-29 15:56:32 +0100

    console logging via pipe

--- operations.sh
+++ operations.sh
@@ -10,5 +10,10 @@ export LD_LIBRARY_PATH=/opt/kakadu/$KAKADULIBNAME/lib/Linux-x86-64-gcc
 
 envsubst < /etc/lighttpd/lighttpd-1.conf.template > /etc/lighttpd/lighttpd-1.conf
 
+# Allow `www-data` user to write to /dev/stderr
+mkfifo -m 600 /tmp/logpipe
+chown www-data:www-data /tmp/logpipe
+cat <> /tmp/logpipe 1>&2 &
+
 echo starting lighttpd
 lighttpd -D -f /etc/lighttpd/lighttpd-1.conf
